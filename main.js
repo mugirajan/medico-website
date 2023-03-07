9a50a18812a8f5845a149d6fe4db4f39a322c69e
mugilavathi <mugirajan95@gmail.com>
2023-03-07 13:56:42 +0530

    add

---
+++ main.js
@@ -0,0 +1,37 @@
+$('.gallery-slider').owlCarousel({
+    autoplay:true,
+	center:true,
+	items:1,
+    loop:true,
+    margin:20,
+    responsive:{
+        0:{
+            items:3
+        },
+		600:{
+            items:3
+        },
+		1000:{
+            items:5
+        }
+    }
+})
+
+$('.testmonals-slide').owlCarousel({
+    autoplay:true,
+	center:true,
+	items:1,
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+		600:{
+            items:1
+        },
+		1000:{
+            items:3
+        }
+    }
+})
